f8dff413c3f6247a8b21938dfd3fb543a293fdfe
YangLX <yanglx725@qq.com>
2021-04-25 20:53:14 +0800

    build template

--- src/picker.vue
+++ src/picker.vue
@@ -1,11 +1,41 @@
 <template>
-  <div></div>
+  <div class="lx-picker">
+    <input type="text" />
+    <div class="picker-wrap">
+      <table class="date-picker">
+        <thead>
+          <tr class="date-head">
+            <th colspan="4">
+              <span class="btn-prev"></span>
+              <span class="show-year"></span>
+              <span class="btn-next"></span>
+            </th>
+            <th colspan="3">
+              <span class="btn-prev"></span>
+              <span class="show-month"></span>
+              <span class="btn-next"></span>
+            </th>
+          </tr>
+          <tr class="date-days">
+            <th v-for="day in days" :key="day">{{ day }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      days: [],
+    };
   },
 
   components: {},
@@ -17,4 +47,4 @@ export default {
   methods: {},
 };
 </script>
-<style lang="scss"></style>
+<style scoped></style>
